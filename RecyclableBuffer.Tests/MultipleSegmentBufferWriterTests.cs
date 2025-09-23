4708c099078ed77dcab0fdd5fd77fb5758a42e25
陈国伟 <366193849@qq.com>
2025-09-23 10:01:26 +0800

    更新tests

--- RecyclableBuffer.Tests/MultipleSegmentBufferWriterTests.cs
+++ RecyclableBuffer.Tests/MultipleSegmentBufferWriterTests.cs
@@ -5,77 +5,131 @@ namespace RecyclableBuffer.Tests
     public class MultipleSegmentBufferWriterTests
     {
         [Fact]
-        public void Constructor_Default_UsesSharedPool()
+        public void Constructor_InitializesWithDefaultPool()
         {
-            using var writer = new RecyclableBuffer.MultipleSegmentBufferWriter();
-            Assert.NotNull(writer);
-        }
-
-        [Fact]
-        public void Constructor_WithPool_ThrowsOnNull()
-        {
-            Assert.Throws<ArgumentNullException>(() => new RecyclableBuffer.MultipleSegmentBufferWriter(null!));
-        }
-
-        [Fact]
-        public void GetMemory_ReturnsWritableMemory()
-        {
-            using var writer = new RecyclableBuffer.MultipleSegmentBufferWriter();
-            var memory = writer.GetMemory(16);
-            Assert.True(memory.Length >= 16);
-        }
-
-        [Fact]
-        public void GetSpan_ReturnsWritableSpan()
-        {
-            using var writer = new RecyclableBuffer.MultipleSegmentBufferWriter();
-            var span = writer.GetSpan(32);
-            Assert.True(span.Length >= 32);
-        }
-
-        [Fact]
-        public void Advance_UpdatesWrittenBuffer()
-        {
-            using var writer = new RecyclableBuffer.MultipleSegmentBufferWriter();
-            var span = writer.GetSpan(8);
-            span[0] = 42;
-            writer.Advance(1);
-
-            var sequence = writer.WrittenSequence;
-            Assert.Equal(1, sequence.Length);
-            Assert.Equal(42, sequence.First.Span[0]);
-        }
-
-        [Fact]
-        public void WrittenBuffer_EmptyWhenNoData()
-        {
-            using var writer = new RecyclableBuffer.MultipleSegmentBufferWriter();
+            using var writer = new MultipleSegmentBufferWriter();
             Assert.Equal(0, writer.WrittenSequence.Length);
         }
 
         [Fact]
-        public void AsStream_WritesData_VerifyWithWrittenBuffer()
+        public void Constructor_ThrowsArgumentNullException_WhenPoolIsNull()
         {
-            using var writer = new RecyclableBuffer.MultipleSegmentBufferWriter();
-            using var stream = writer.AsWritableStream();
-
-            var data = new byte[] { 1, 2, 3, 4 };
-            stream.Write(data, 0, data.Length);
-
-            var sequence = writer.WrittenSequence;
-            Assert.Equal(data.Length, sequence.Length);
-            Assert.Equal(data, sequence.ToArray());
+            Assert.Throws<ArgumentNullException>(() => new MultipleSegmentBufferWriter(null!));
         }
 
         [Fact]
-        public void Dispose_ReleasesBuffers()
+        public void Advance_ThrowsInvalidOperationException_IfNoBuffer()
         {
-            using var writer = new RecyclableBuffer.MultipleSegmentBufferWriter();
-            writer.GetSpan(8);
-            writer.Dispose();
+            using var writer = new MultipleSegmentBufferWriter();
+            Assert.Throws<InvalidOperationException>(() => writer.Advance(1));
+        }
 
-            // Dispose again should not throw
+        [Fact]
+        public void Advance_IncreasesWrittenLength()
+        {
+            using var writer = new MultipleSegmentBufferWriter();
+            var span = writer.GetSpan(4);
+            span[0] = 1;
+            span[1] = 2;
+            span[2] = 3;
+            span[3] = 4;
+            writer.Advance(4);
+
+            Assert.Equal(4, writer.WrittenSequence.Length);
+            Assert.Equal([1, 2, 3, 4], writer.WrittenSequence.ToArray());
+        }
+
+        [Fact]
+        public void GetMemory_AllocatesBuffer_WhenNoneExists()
+        {
+            using var writer = new MultipleSegmentBufferWriter();
+            var mem = writer.GetMemory(8);
+            Assert.True(mem.Length >= 8);
+        }
+
+        [Fact]
+        public void GetMemory_ExpandsBuffer_WhenInsufficientSpace()
+        {
+            using var writer = new MultipleSegmentBufferWriter();
+            writer.GetMemory(4);
+            writer.Advance(4);
+
+            var mem = writer.GetMemory(10);
+            Assert.True(mem.Length >= 10);
+        }
+
+        [Fact]
+        public void GetSpan_AllocatesBuffer_WhenNoneExists()
+        {
+            using var writer = new MultipleSegmentBufferWriter();
+            var span = writer.GetSpan(8);
+            Assert.True(span.Length >= 8);
+        }
+
+        [Fact]
+        public void GetSpan_ExpandsBuffer_WhenInsufficientSpace()
+        {
+            using var writer = new MultipleSegmentBufferWriter();
+            writer.GetSpan(4);
+            writer.Advance(4);
+
+            var span = writer.GetSpan(10);
+            Assert.True(span.Length >= 10);
+        }
+
+        [Fact]
+        public void WrittenSequence_ReturnsCorrectData_AcrossMultipleSegments()
+        {
+            using var writer = new MultipleSegmentBufferWriter();
+            var span1 = writer.GetSpan(4);
+            span1[0] = 10;
+            span1[1] = 20;
+            span1[2] = 30;
+            span1[3] = 40;
+            writer.Advance(4);
+
+            var span2 = writer.GetSpan(4);
+            span2[0] = 50;
+            span2[1] = 60;
+            span2[2] = 70;
+            span2[3] = 80;
+            writer.Advance(4);
+
+            var seq = writer.WrittenSequence;
+            Assert.Equal(8, seq.Length);
+            Assert.Equal([10, 20, 30, 40, 50, 60, 70, 80], seq.ToArray());
+        }
+
+        [Fact]
+        public void Advance_ThrowsInvalidOperationException_AfterDispose()
+        {
+            var writer = new MultipleSegmentBufferWriter();
             writer.Dispose();
+            Assert.Throws<InvalidOperationException>(() => writer.Advance(1));
+        }
+
+        [Fact]
+        public void GetMemory_ThrowsObjectDisposedException_AfterDispose()
+        {
+            var writer = new MultipleSegmentBufferWriter();
+            writer.Dispose();
+            Assert.Throws<ObjectDisposedException>(() => writer.GetMemory(1));
+        }
+
+        [Fact]
+        public void GetSpan_ThrowsObjectDisposedException_AfterDispose()
+        {
+            var writer = new MultipleSegmentBufferWriter();
+            writer.Dispose();
+            Assert.Throws<ObjectDisposedException>(() => writer.GetSpan(1));
+        }
+
+        [Fact]
+        public void Dispose_CanBeCalledMultipleTimes()
+        {
+            var writer = new MultipleSegmentBufferWriter();
+            writer.Dispose();
+            writer.Dispose(); // Should not throw
         }
     }
 }
--- RecyclableBuffer.Tests/SingleSegmentBufferWriterTests.cs
+++ RecyclableBuffer.Tests/SingleSegmentBufferWriterTests.cs
@@ -5,84 +5,85 @@ namespace RecyclableBuffer.Tests
     public class SingleSegmentBufferWriterTests
     {
         [Fact]
-        public void Constructor_WithInitialCapacity_ShouldAllocateBuffer()
+        public void Constructor_InitializesBufferWithMinimumLength()
         {
-            var writer = new SingleSegmentBufferWriter(16);
+            using var writer = new SingleSegmentBufferWriter(16);
             Assert.True(writer.WrittenSpan.Length == 0);
             Assert.True(writer.WrittenMemory.Length == 0);
-            writer.Dispose();
+            Assert.True(writer.WrittenSequence.Length == 0);
+        }
+
+        [Fact]
+        public void Constructor_ThrowsArgumentNullException_WhenPoolIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SingleSegmentBufferWriter(8, null!));
         }
 
         [Fact]
         public void Constructor_WithPoolAndCapacity_ShouldAllocateBuffer()
         {
             var pool = ByteArrayPool.Shared;
-            var writer = new SingleSegmentBufferWriter(32, pool);
+            using var writer = new SingleSegmentBufferWriter(32, pool);
             Assert.True(writer.WrittenSpan.Length == 0);
-            writer.Dispose();
         }
 
         [Fact]
-        public void GetMemory_And_Advance_ShouldWriteData()
+        public void Advance_IncreasesWrittenLength()
         {
-            var writer = new SingleSegmentBufferWriter(8);
-            var memory = writer.GetMemory(4);
-            Assert.True(memory.Length >= 4);
-
-            var span = memory.Span;
+            using var writer = new SingleSegmentBufferWriter(8);
+            var span = writer.GetSpan(4);
             span[0] = 1;
             span[1] = 2;
             span[2] = 3;
             span[3] = 4;
-
             writer.Advance(4);
 
-            var written = writer.WrittenSpan;
-            Assert.Equal([1, 2, 3, 4], written.ToArray());
-            writer.Dispose();
+            Assert.Equal(4, writer.WrittenSpan.Length);
+            Assert.Equal(4, writer.WrittenMemory.Length);
+            Assert.Equal(4, writer.WrittenSequence.Length);
+            Assert.Equal([1, 2, 3, 4], writer.WrittenSpan.ToArray());
         }
 
         [Fact]
-        public void GetSpan_And_Advance_ShouldWriteData()
+        public void GetMemory_ExpandsBuffer_WhenInsufficientSpace()
         {
-            var writer = new SingleSegmentBufferWriter(8);
-            var span = writer.GetSpan(4);
-            Assert.True(span.Length >= 4);
+            using var writer = new SingleSegmentBufferWriter(4);
+            writer.GetMemory(4);
+            writer.Advance(4);
 
+            var mem = writer.GetMemory(10);
+            Assert.True(mem.Length >= 10);
+        }
+
+        [Fact]
+        public void GetSpan_ExpandsBuffer_WhenInsufficientSpace()
+        {
+            using var writer = new SingleSegmentBufferWriter(4);
+            writer.GetSpan(4);
+            writer.Advance(4);
+
+            var span = writer.GetSpan(10);
+            Assert.True(span.Length >= 10);
+        }
+
+        [Fact]
+        public void WrittenSequence_ReturnsCorrectData()
+        {
+            using var writer = new SingleSegmentBufferWriter(8);
+            var span = writer.GetSpan(4);
             span[0] = 10;
             span[1] = 20;
             span[2] = 30;
             span[3] = 40;
-
             writer.Advance(4);
 
-            var written = writer.WrittenSpan;
-            Assert.Equal([10, 20, 30, 40], written.ToArray());
-            writer.Dispose();
+            var seq = writer.WrittenSequence;
+            Assert.Equal(4, seq.Length);
+            Assert.Equal([10, 20, 30, 40], seq.ToArray());
         }
 
         [Fact]
-        public void GetMemory_ShouldAutoResize_WhenInsufficient()
-        {
-            var writer = new SingleSegmentBufferWriter(2);
-            var memory = writer.GetMemory(10);
-            Assert.True(memory.Length >= 10);
-            writer.Dispose();
-        }
-
-        [Fact]
-        public void GetSpan_ShouldAutoResize_WhenInsufficient()
-        {
-            var writer = new SingleSegmentBufferWriter(2);
-            writer.Write(new byte[] { 1 });
-
-            var span = writer.GetSpan(127);
-            Assert.True(span.Length >= 127);
-            writer.Dispose();
-        }
-
-        [Fact]
-        public void Advance_ShouldThrow_WhenDisposed()
+        public void Advance_ThrowsObjectDisposedException_AfterDispose()
         {
             var writer = new SingleSegmentBufferWriter(8);
             writer.Dispose();
@@ -90,7 +91,7 @@ namespace RecyclableBuffer.Tests
         }
 
         [Fact]
-        public void GetMemory_ShouldThrow_WhenDisposed()
+        public void GetMemory_ThrowsObjectDisposedException_AfterDispose()
         {
             var writer = new SingleSegmentBufferWriter(8);
             writer.Dispose();
@@ -98,7 +99,7 @@ namespace RecyclableBuffer.Tests
         }
 
         [Fact]
-        public void GetSpan_ShouldThrow_WhenDisposed()
+        public void GetSpan_ThrowsObjectDisposedException_AfterDispose()
         {
             var writer = new SingleSegmentBufferWriter(8);
             writer.Dispose();
@@ -106,11 +107,11 @@ namespace RecyclableBuffer.Tests
         }
 
         [Fact]
-        public void Dispose_ShouldBeIdempotent()
+        public void Dispose_CanBeCalledMultipleTimes()
         {
             var writer = new SingleSegmentBufferWriter(8);
             writer.Dispose();
-            writer.Dispose(); // ²»Ó¦Å×Òì³£
+            writer.Dispose(); // Should not throw
         }
     }
 }
